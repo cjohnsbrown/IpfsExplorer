0e865158539644dcdeb84a8a353c35f3577de1c4
Cameron <camjohnsbrown@outlook.com>
2018-12-19 18:30:13 -0500

    Intital commit

---
+++ IpfsExplorer/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using IpfsExplorer.Core;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IpfsExplorer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<PinnedItem> items = new ObservableCollection<PinnedItem>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            lvPinned.ItemsSource = items;
+            Run();
+		
+        }
+
+        public async Task Run() {
+
+            var path = @"C:\Users\cameron\mine\config.txt";
+            var proxy = new IpfsProxy("http://10.0.0.129:5001");
+            string hash = await proxy.AddFileAsync(path);
+            items.Add(new PinnedItem(hash, new System.IO.FileInfo(path)));
+
+        }
+    }
+}
---
+++ IpfsExplorer/PinnedItem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Linq;
+
+namespace IpfsExplorer
+{
+    class PinnedItem { 
+        public string Hash { get; set; }
+        public string FileName { get; set; }
+        public long Size { get; set; }
+        public DateTime PinDate { get; set; }
+        public bool IsDirectory { get; set; }
+
+        public PinnedItem() { }
+
+        public PinnedItem(string hash, FileInfo info) {
+            Hash = hash;
+            FileName = info.Name;
+            Size = info.Length;
+            PinDate = DateTime.Now;
+            IsDirectory = false;
+        }
+
+        public PinnedItem(string hash, DirectoryInfo info) {
+            Hash = hash;
+            FileName = info.Name;
+            PinDate = DateTime.Now;
+            IsDirectory = true;
+
+            Size = GetDirectorySize(info);
+        }
+
+        private long GetDirectorySize(DirectoryInfo info) {
+            long size = info.EnumerateFiles().Sum(f => f.Length);
+
+            foreach (var d in info.EnumerateDirectories()) {
+                size += GetDirectorySize(d);
+            }
+            return size;
+        }
+        
+    }
+}

eb9c8278476a9ac6f4b90c61c5ccaf76424f97b3
Cameron <camjohnsbrown@outlook.com>
2018-12-27 17:29:33 -0500

    add click handlers for buttons

--- IpfsExplorer/MainWindow.xaml.cs
+++ IpfsExplorer/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using IpfsExplorer.Core;
+using Microsoft.Win32;
+using Microsoft.WindowsAPICodePack.Dialogs;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,22 +26,62 @@ namespace IpfsExplorer
     /// </summary>
     public partial class MainWindow : Window
     {
-        ObservableCollection<PinnedItem> items = new ObservableCollection<PinnedItem>();
+        ObservableCollection<PinnedItem> PinnedItems = new ObservableCollection<PinnedItem>();
+        IpfsProxy Proxy;
         public MainWindow()
         {
             InitializeComponent();
-
-            lvPinned.ItemsSource = items;
-            Run();
+            lvPinned.ItemsSource = PinnedItems;
+            CollectionView view = (CollectionView)CollectionViewSource.GetDefaultView(lvPinned.ItemsSource);
+            view.SortDescriptions.Add(new SortDescription(nameof(PinnedItem.FileName), ListSortDirection.Ascending));
+            Proxy = new IpfsProxy("http://10.0.0.129:5001");
 		
         }
 
-        public async Task Run() {
+        private async void BtnAddFile_Click(object sender, RoutedEventArgs e) {
+            var dialog = new OpenFileDialog();
+            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            if (dialog.ShowDialog() == false) {
+                return;
+            }
+            try {
+                var item = await Proxy.AddFileAsync(dialog.FileName);
+                PinnedItems.Add(item);
+            } catch (Exception ex) {
+                MessageBox.Show($"Error adding file: {ex.Message}");
+            }
+        }
 
-            var path = @"C:\Users\cameron\mine\config.txt";
-            var proxy = new IpfsProxy("http://10.0.0.129:5001");
-            string hash = await proxy.AddFileAsync(path);
-            items.Add(new PinnedItem(hash, new System.IO.FileInfo(path)));
+        private async void BtnAddFolder_Click(object sender, RoutedEventArgs e) {
+            var dialog = new CommonOpenFileDialog();
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok) {
+                var item = await Proxy.AddDirectoryAsync(dialog.FileName);
+                PinnedItems.Add(item);
+            }
+        }
+
+        private void ListViewItem_MouseDoubleClick(object sender, MouseButtonEventArgs e) {
+            var row = sender as ListViewItem;
+            if (row != null && row.IsSelected) {
+                var item = row.Content as PinnedItem;
+                MessageBox.Show(item.Hash);
+            }
+        }
+
+        private void BtnDownload_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void BtnPin_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void BtnRemove_Click(object sender, RoutedEventArgs e) {
+
+        }
+
+        private void BtnPref_Click(object sender, RoutedEventArgs e) {
 
         }
     }

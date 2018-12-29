4f751fffc050d4450f1ff7661eebbaed00f3b2b9
Cameron <camjohnsbrown@outlook.com>
2018-12-29 16:31:09 -0500

    implment INotifyPropertyChanged interface

--- IpfsExplorer.Core/PinnedItem.cs
+++ IpfsExplorer.Core/PinnedItem.cs
@@ -4,18 +4,35 @@ using System.IO;
 using System.Text;
 using System.Linq;
 using Ipfs;
+using System.ComponentModel;
 
 namespace IpfsExplorer
 {
-    public class PinnedItem {
+    public class PinnedItem : INotifyPropertyChanged {
 
         private readonly string[] Units = new[] { "kB", "MB", "GB", "TB" };
 
+        private string name;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public string Hash { get; set; }
-        public string FileName { get; set; }
+
+        public string FileName {
+            get {
+                return name;
+            }
+            set {
+                name = value;
+                NotifyPropertyChanged(nameof(FileName));
+            }
+        }
+
         public string Size { get; set; }
         public DateTime PinDate { get; set; }
         public bool IsDirectory { get; set; }
+
+        public List<PinnedItem> DirectoryContents { get; private set; }
 
         public PinnedItem() { }
 
@@ -24,7 +41,7 @@ namespace IpfsExplorer
             Hash = node.Id.Hash.ToBase58();
             FileName = node.ToLink().Name;
             PinDate = DateTime.Now;
-            IsDirectory = node.IsDirectory;
+            IsDirectory = true;
 
             //Convert size to human readable string
             var i = -1;
@@ -36,7 +53,34 @@ namespace IpfsExplorer
 
             Size = $"{Math.Max(displaySize, 0.1).ToString("0.#")} {Units[i]}";
 
+            DirectoryContents = new List<PinnedItem>(node.Links.Count());
+            foreach (var link in node.Links) {
+                DirectoryContents.Add(new PinnedItem(link));
+            }
+
         }
-        
+
+        public PinnedItem(IFileSystemLink link) {
+            Hash = link.Id.Hash.ToBase58();
+            FileName = link.Name;
+            PinDate = DateTime.Now;
+            IsDirectory = false;
+
+            //Convert size to human readable string
+            var i = -1;
+            double displaySize = link.Size;
+            do {
+                displaySize /= 1024;
+                i++;
+            } while (displaySize > 1024 && i < Units.Length);
+
+            Size = $"{Math.Max(displaySize, 0.1).ToString("0.#")} {Units[i]}";
+        }
+
+        private void NotifyPropertyChanged(string propName) {
+            if (PropertyChanged !=null) {
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+            }
+        }
     }
 }
